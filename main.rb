ce65f3ff847d1892e9181ba3669ebb0f7a0f0fb5
akumaginkou <ryoryo939@gmail.com>
2021-10-19 14:20:55 +0900

    test: 数を文字列にして返す

--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 require 'minitest/autorun'
 
-class HelloTest < Minitest::Test
-  def test_greeting
-    assert_equal 'hello world!!!', greeting
+class FizzBuzzTest < Minitest::Test
+    def test_数字を渡したら文字列を返す
+        assert_equal '1', FizzBuzz.generate(1)
+        assert_equal '2', FizzBuzz.generate(2)
+        assert_equal '3', FizzBuzz.generate(3)
+        assert_equal '4', FizzBuzz.generate(4)
+        assert_equal '5', FizzBuzz.generate(5)
+      end
   end
-end
-
-def greeting
-  'hello world'
-end
+  class FizzBuzz
+    def self.generate(n)
+      n.to_s
+    end
+  end
