28fa5122549c05b520762b949e333b53da38c323
akumaginkou <ryoryo939@gmail.com>
2021-10-26 13:34:26 +0900

    refactor: メソッドのインライン化

--- main.rb
+++ main.rb
@@ -3,26 +3,34 @@ Minitest::Reporters.use!
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
+  describe 'FizzBuzz' do
     def setup
       @fizzbuzz = FizzBuzz
     end
 
-    def test_1を渡したら文字列1を返す
-      assert_equal '1', @fizzbuzz.generate(1)
-    end
-
-    def test_2を渡したら文字列2を返す
-      assert_equal '2', @fizzbuzz.generate(2)
-    end
-
-    def test_3を渡したら文字列Fizzを返す
+    describe '三の倍数の場合' do
+      def test_3を渡したら文字列Fizzを返す
         assert_equal 'Fizz', @fizzbuzz.generate(3)
       end
+    end
 
-    def test_5を渡したら文字列Buzzを返す
+    describe '五の倍数の場合' do
+      def test_5を渡したら文字列Buzzを返す
         assert_equal 'Buzz', @fizzbuzz.generate(5)
       end
+    end
+
+    describe 'その他の場合' do
+      def test_1を渡したら文字列1を返す
+        assert_equal '1', @fizzbuzz.generate(1)
+      end
+
+      def test_2を渡したら文字列2を返す
+        assert_equal '2', @fizzbuzz.generate(2)
+      end
+    end
   end
+end
   class FizzBuzz
     def self.generate(number)
       result = number.to_s
